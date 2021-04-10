35539d0019f93d1fcd45a3ec7372217e409e7015
Eslicdm <eslicdm1@gmail.com>
2021-04-10 11:51:54 -0300

    registerActivityForResult replaces startActivityForResult(deprecated)

--- README.md
+++ README.md
@@ -4,3 +4,12 @@ App for training write code
 
 ## Description
 A button takes you to another activity that allows you to write a text and return to a previous activity and show it within a textview. Counter with data binding, live data and viewmodel. Prevent the recreation of the activity with configchanges(manifest).
+
+## Preview
+<img src="https://user-images.githubusercontent.com/53240638/114271289-037dc000-99e7-11eb-860f-9da65bfbe0b3.png" width="200" height="350"> <img src="https://user-images.githubusercontent.com/53240638/114271296-0d9fbe80-99e7-11eb-85bf-02c73a1e2136.png" width="200" height="350"> <img src="https://user-images.githubusercontent.com/53240638/114271305-11cbdc00-99e7-11eb-803a-b8ea0c32313e.png" width="200" height="350">
+
+## Deprecations
+
+startActivityForResult and onActivityResult -> registerForActivityResult
+
+<img src="https://user-images.githubusercontent.com/53240638/114271575-7176b700-99e8-11eb-8638-45a1fa5c6d01.png" width="350" height="200"> .. <img src="https://user-images.githubusercontent.com/53240638/114273886-5c9f2100-99f2-11eb-827a-cf305e231d3b.png" width="550" height="200">
--- app/src/main/java/com/eslirodrigues/intentconversationanddatabindinglivedata/MainActivity.kt
+++ app/src/main/java/com/eslirodrigues/intentconversationanddatabindinglivedata/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.result.contract.ActivityResultContracts
 import com.eslirodrigues.intentconversationanddatabindinglivedata.countdatabinding.CountActivity
 import com.eslirodrigues.intentconversationanddatabindinglivedata.databinding.ActivityMainBinding
 
@@ -13,30 +14,22 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    companion object {
-        const val REQUEST_CODE = 1
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val launchActivity = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data: Intent? = result.data
+                val textRequest: String? = data?.getStringExtra("key")
+                binding.textViewReceived.text = textRequest
+            }
+        }
 
         binding.buttonRequest.setOnClickListener {
             val intent = Intent(this, SecondActivity::class.java)
-            startActivityForResult(intent, REQUEST_CODE)
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == REQUEST_CODE) {
-            if (resultCode == Activity.RESULT_OK) {
-                val textResponse = data?.getStringExtra("key")
-                binding.textViewReceived.text = textResponse
-            }
+            launchActivity.launch(intent)
         }
     }
 
